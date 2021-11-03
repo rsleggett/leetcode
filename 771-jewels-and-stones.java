4adb38bfad2fdf865139efd872b7c70b7795515f
Rob <rsleggett@googlemail.com>
2021-11-03 07:55:15 +0100

    Fastest version with best memory

--- 771-jewels-and-stones.java
+++ 771-jewels-and-stones.java
@@ -1,14 +1,14 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int[] ascii = new int[256];
+        boolean[] ascii = new boolean[256];
         for(int i = 0; i < jewels.length(); i++) //O(n)
         {
-            ascii[jewels.charAt(i)] = 1; //O(1)
+            ascii[jewels.charAt(i)] = true; //O(1)
         }
         int count = 0;
         for(int i = 0; i < stones.length(); i++) //O(m)
         {
-            count += ascii[stones.charAt(i)]; // O(1)
+            count += ascii[stones.charAt(i)] ? 1:0; // O(1)
         }
         return count;
     }
