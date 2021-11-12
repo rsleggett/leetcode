3d1ea5adf4f730f89d44a41065bc2ad4524de7f9
Rob <rsleggett@googlemail.com>
2021-11-12 16:28:40 +0100

    Update 1678-goal-parser-interpretation.java

--- 1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation.java
@@ -1,5 +1,25 @@
 class Solution {
     public String interpret(String command) {
-        return command.replace("()","o").replace("(al)","al");    
+         StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < command.length();)
+        {
+            char curr = command.charAt(i);
+            if (curr == 'G')
+            {
+                sb.append(curr);
+                i++;
+            }
+            else if(i + 1 < command.length() && command.charAt(i+1) == ')')
+            {
+                sb.append("o");
+                i+=2;
+            }
+            else
+            {
+                sb.append("al");
+                i+=4;
+            }
+        }
+        return sb.toString();       
     }
 }
