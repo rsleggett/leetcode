6d9f64cbc23f9a04253ba49c52253465352e3f47
Rob <rsleggett@googlemail.com>
2021-11-20 20:15:50 +0100

    Update 1282-group-the-people-given-the-group-size-they-belong-to.java

--- 1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -3,30 +3,16 @@ class Solution {
         var list = new ArrayList<List<Integer>>();
         var hash = new HashSet<Integer>();
         
-        // for i groupSizes
-        // if index is already in hash
-            // contiue
-        // if groupSizes[i] == 1
-            // add the index to the list
-            // continue
-        // if groupSize[i] > 1
-            // add to a list
-            // for the rest of the group 
-                // collect other index that are the same until group is full
-                // continue
         for (int i = 0; i < groupSizes.length; i++)
         {
             if (hash.contains(i))
             {
-                System.out.println("hash contains " + i);
                 continue;
             }
+            
             int groupSize = groupSizes[i];
             
-            System.out.println("i=" + i);
-            System.out.println("groupSize=" + groupSize);
             hash.add(i);
-            System.out.println("added to hash " + i);
             List<Integer> group = new ArrayList<Integer>();
             group.add(i);
             
@@ -34,11 +20,8 @@ class Solution {
             {
                 for (int j = i+1; j < groupSizes.length;j++)
                 {
-                    System.out.println("j="+j);
-                    System.out.println("groupSizes[j]=" + groupSizes[j]);
                     if (groupSizes[i] == groupSizes[j])
                     {
-                        System.out.println("added to group and hash j=" + j);
                         group.add(j);
                         hash.add(j);
 
